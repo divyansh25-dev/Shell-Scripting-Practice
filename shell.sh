612b5dede884b0c2d59e1c9265d09bd98493b760
Divyansh <divyansh25.panwar@gmail.com>
2022-02-18 13:26:24 +0530

    User addition fuctionality added

--- shell.sh
+++ shell.sh
@@ -2,4 +2,10 @@
 pwd #To see what directory we are in!
 ifconfig | grep inet #To find the ip address
 sudo cat  /var/log/apache2/apache2.logs #This command will give us the logs of apache server directly
+#The following script from now on will be done to automate user addition and some modifications for user permissions
+
+echo "Enter Your Username:"
+read username
+sudo useradd -m $username 
+
 
