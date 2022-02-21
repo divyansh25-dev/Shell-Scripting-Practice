df73335edf993752c33281147c4db4afc47d4733
Divyansh <divyansh25.panwar@gmail.com>
2022-02-21 10:54:33 +0530

    Group add

--- shell.sh
+++ shell.sh
@@ -13,8 +13,10 @@ echo "Do you want to be added in the group?Please enter yes or no"
 read n
 yes=$(echo $n | tr -s '[:upper:]' '[:lower:]')
 if [[  "$n" = "yes"  ]] ; then
-  sudo groupadd $username
-  sudo usermod -s -G $username $username
+  echo "Type the name of the group you want to make?"
+  read grupnem 
+  sudo groupadd $grupnem
+  sudo usermod -s -G $grupnem $username
   echo "Congratulations you are added to a group!"
 else
   echo "You are not added to any group."
